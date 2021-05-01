97361775ab83f8d036afaf27ebad9eb4ef6a72e6
yogapratama24 <ypramsatya@gmail.com>
2021-05-01 17:24:08 +0700

    Function Penghitung Jumlah Kata

---
+++ Quiz-1/index.js
@@ -0,0 +1,16 @@
+console.log("Soal Nomor 1");
+
+function jumlah_kata(params){
+    let first = params.split(" ").length
+    console.log(first);
+    
+}
+
+var kalimat_1 = "Halo nama saya Muhammad Iqbal Mubarok"
+var kalimat_2 = "Saya Iqbal"
+
+jumlah_kata(kalimat_1)
+jumlah_kata(kalimat_2)
+
+
+
